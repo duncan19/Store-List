b8d2ce76597524f1d34cea9c4341b09175510ff3
Student <example@example.com>
2019-08-19 15:38:42 -0700

    Add in a non working js for the list

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var blanks = ["item1", "item2", "item3", "item4", "item5", "item6"];
+
+    blanks.forEach(function(blank) {
+      var userinput = $("input#" + blank).val();
+      $("." + blank).text(userinput);
+    });
+
+    blanks.sort();
+    alert(userinput);
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
